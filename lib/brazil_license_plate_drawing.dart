c729808d20d5261588ce535b947d9ce5e5646ce7
Thad Carnevalli <tcarnevalli@gmail.com>
2021-03-11 16:15:35 -0300

    project classes were made public

--- lib/brazil_license_plate_drawing.dart
+++ lib/brazil_license_plate_drawing.dart
@@ -1,3 +1,6 @@
 library brazil_license_plate_drawing;
 
+export 'brazil_plate_category.dart' show BrazilPlateCategory;
+export 'two_letters_plate.dart' show TwoLettersPlate;
+export 'three_letters_plate.dart' show ThreeLettersPlate;
 export 'mercosul_plate.dart' show MercosulPlate;
