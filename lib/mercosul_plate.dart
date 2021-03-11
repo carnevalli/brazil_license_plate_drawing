04eb187afcf2787e4116cca62cf99b5e561e6a31
Thad Carnevalli <tcarnevalli@gmail.com>
2021-03-11 09:44:01 -0300

    documentation has been added

--- lib/mercosul_plate.dart
+++ lib/mercosul_plate.dart
@@ -1,19 +1,114 @@
 import 'package:flutter/material.dart';
 
 class MercosulPlate extends StatelessWidget {
+  /// The letters and numbers drawn in the licence plate
   final String plate;
-  final double width;
-  final double height;
-  static const double _heightRelation = 235 / 724;
-  static const double _leftBorderRelation = 15 / 724;
-  static const double _rightBorderRelation = 13 / 724;
-  static const double _verticalBorderRelation = 14 / 235;
-  static const double _localityContainerHeightRelation = 55 / 235;
-  static const double _localityContainerLettersRelation = 28 / 235;
-  static const double _lettersHeightRelation = 124 / 235;
 
-  MercosulPlate(this.plate, [this.width = 300])
-      : height = _heightRelation * width;
+  /// The width that the whole widget will take. If null, the width value
+  /// will be infered from the height value. If height is null, then the
+  /// _defaultWidth value will be considered.
+  final double? width;
+
+  /// The height that the whole widget will take. If left null, then the original
+  /// proportion factor will be used to calculate this value from the width.
+  final double? height;
+
+  /// The string that represents the text which will be positioned at the top
+  /// of the licence plate, generaly the name of the country.
+  /// Always converted to uppercase.
+  /// Defaults to "BRASIL"
+  final String countryText;
+
+  /// The two letters printed at the bottom left position.
+  /// Always converted to uppercase.
+  /// Defaults to "BR"
+  final String countryAcronymLetters;
+
+  /// The name of the image asset that will be placed in the top right corner.
+  /// Considers a relative path from the root of the `assets` folder.
+  /// Defaults to `brazil.png`
+  final String countryFlagAsset;
+
+  /// The path of the logo asset that will be placed in the top left corner.
+  /// Considers a relative path from the root of the `assets` folder.
+  /// Defaults to `merco.png`
+  final String mercosulLogoAsset;
+
+  // The following constants are the proportion factors from the sizes of a
+  // real car license plate.
+
+  /// Relation between width and height
+  static const double _heightRelation = 235 / 724;
+
+  /// Relation between the width of the left border and the entire plate width.
+  static const double _leftBorderRelation = 15 / 724;
+
+  /// Relation between the width of the right border and the entire plate width.
+  static const double _rightBorderRelation = 13 / 724;
+
+  /// Relation between the height of top and bottom borders
+  /// and the entire plate height.
+  static const double _verticalBorderRelation = 14 / 235;
+
+  /// Relation between the height of the locality header container and the entire
+  /// plate height.
+  static const double _localityContainerHeightRelation = 55 / 235;
+
+  /// Relation between the height of the locality header text font and the entire
+  /// plate height.
+  static const double _localityContainerLettersRelation = 28 / 235;
+
+  /// Relation between the height of the letters of the license plate and the entire
+  /// license plate height.
+  static const double _lettersHeightRelation = 124 / 235;
+  // Default plate width if neither width or height values were provided for the
+  // class constructor
+  static const double _defaultWidth = 300;
+
+  /// Evaluates the width value that will be used to draw the widget.
+  /// If a value is passed to the constructor, then this value will be used.
+  /// Otherwise, it checks if a value has been passed to height and calculates
+  /// the width using the original proportion factor. If both width and height are
+  /// nulls, then the value setted for _defaultWidth will be used.
+  double get realWidth {
+    if (width != null) {
+      return width!;
+    } else {
+      if (height == null) {
+        return _defaultWidth;
+      } else {
+        return height! * 1 / _heightRelation;
+      }
+    }
+  }
+
+  /// Evaluates the real height that will be considered
+  /// while drawing the widget.
+  /// If a height value was explicitly passed to object's
+  /// constructor, then this value will be used.
+  /// Otherwise, it infers a value for height from the width property,
+  /// consering the original proportional factor.
+  double get realHeight {
+    if (height != null) {
+      return height!;
+    } else {
+      return realWidth * _heightRelation;
+    }
+  }
+
+  /// Class constructor. To obtain the original aspect ratio  of a real
+  /// license plate, only provide a value for width
+  /// OR height, so it will keep the original proportion automatically.
+  /// If a value is provided for these two properties, then the original
+  /// aspect ratio will not be take in account.
+  /// If neither are provided, the value of _defaultWidth will be used.
+  MercosulPlate(this.plate,
+      {this.width,
+      this.height,
+      this.countryText = 'BRASIL',
+      this.countryAcronymLetters = 'BR',
+      this.countryFlagAsset = 'brazil.png',
+      this.mercosulLogoAsset = 'merco.png'});
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +126,7 @@ class MercosulPlate extends StatelessWidget {
     );
   }
 
+  /// Draws the outer borders.
   Widget _externalWrapper({required Widget child}) {
     return Container(
         decoration: BoxDecoration(
@@ -42,19 +138,21 @@ class MercosulPlate extends StatelessWidget {
               offset: Offset(0, 3),
             ),
           ],
-          borderRadius: BorderRadius.circular(15 * (width / 500)),
+          borderRadius: BorderRadius.circular(15 * (realWidth / 500)),
           color: Colors.black,
         ),
         width: width,
         height: height,
         padding: EdgeInsets.fromLTRB(
-            width * _leftBorderRelation,
-            height * _verticalBorderRelation,
-            width * _rightBorderRelation,
-            height * _verticalBorderRelation),
+            realWidth * _leftBorderRelation,
+            realHeight * _verticalBorderRelation,
+            realWidth * _rightBorderRelation,
+            realHeight * _verticalBorderRelation),
         child: child);
   }
 
+  /// Draws the inner content wrapper and places the two letter acronym
+  /// in the bottom left corner.
   Widget _internalWrapper({required Widget child}) {
     return Container(
       decoration: BoxDecoration(
@@ -63,12 +161,12 @@ class MercosulPlate extends StatelessWidget {
       child: Stack(
         children: [
           Positioned(
-            bottom: -10 * (width / 1000),
+            bottom: -10 * (realWidth / 1000),
             left: 0,
-            child: Text('BR',
+            child: Text(countryAcronymLetters.toUpperCase(),
                 style: TextStyle(
                   fontWeight: FontWeight.w800,
-                  fontSize: 60 * (width / 1000),
+                  fontSize: 60 * (realWidth / 1000),
                 )),
           ),
           child,
@@ -77,12 +175,13 @@ class MercosulPlate extends StatelessWidget {
     );
   }
 
+  /// Draws the header which contains the country name and flags.
   Widget _countryTopBar() {
-    final double flagBorderRadius = 3 * (width / 500);
-    final double flagPadding = 2 * (width / 500);
+    final double flagBorderRadius = 3 * (realWidth / 500);
+    final double flagPadding = 2 * (realWidth / 500);
     return Container(
         width: double.infinity,
-        height: height * _localityContainerHeightRelation,
+        height: realHeight * _localityContainerHeightRelation,
         decoration: BoxDecoration(
           color: Color(0xFF003399),
         ),
@@ -90,35 +189,35 @@ class MercosulPlate extends StatelessWidget {
           alignment: Alignment.center,
           children: [
             Positioned(
-              left: 5 * (width / 500),
+              left: 5 * (realWidth / 500),
               child: Container(
                 color: Colors.transparent,
-                height: height * _localityContainerHeightRelation * 0.8,
-                child: Image.asset('merco.png',
+                height: realHeight * _localityContainerHeightRelation * 0.8,
+                child: Image.asset(mercosulLogoAsset,
                     color: Color(0xFF003399),
                     colorBlendMode: BlendMode.lighten),
               ),
             ),
             Text(
-              'BRASIL',
+              countryText.toUpperCase(),
               textAlign: TextAlign.center,
               style: TextStyle(
                   height: 1.9,
-                  fontSize: height * _localityContainerLettersRelation,
+                  fontSize: realHeight * _localityContainerLettersRelation,
                   fontWeight: FontWeight.bold,
                   color: Colors.white),
             ),
             Positioned(
-              right: 5 * (width / 500),
+              right: 5 * (realWidth / 500),
               child: Container(
-                height: height * _localityContainerHeightRelation * 0.8,
+                height: realHeight * _localityContainerHeightRelation * 0.8,
                 child: Container(
                   clipBehavior: Clip.hardEdge,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(flagBorderRadius),
                   ),
                   child: Image.asset(
-                    'brazil.png',
+                    countryFlagAsset,
                     fit: BoxFit.contain,
                   ),
                 ),
@@ -134,26 +233,29 @@ class MercosulPlate extends StatelessWidget {
         ));
   }
 
+  /// Draws the area in which will be printed the main license plate characters.
   Widget _charactersContent() {
     final String letters = plate.substring(0, 3);
     final String numbers = plate.substring(3, 7);
     return Container(
-      height: height * _lettersHeightRelation,
+      height: realHeight * _lettersHeightRelation,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          _getPlateChars(letters, height * _lettersHeightRelation),
-          _getPlateChars(numbers, height * _lettersHeightRelation),
+          _getPlateChars(letters, realHeight * _lettersHeightRelation),
+          _getPlateChars(numbers, realHeight * _lettersHeightRelation),
         ],
       ),
     );
   }
 
+  /// Styles the main license plate characters
   Text _getPlateChars(String chars, [double fontSize = 40]) {
     return Text(
       chars.toUpperCase(),
       style: TextStyle(
+        height: 0.45,
         fontSize: fontSize,
         letterSpacing: 4 * (fontSize / 98),
         fontFamily: 'fe',
